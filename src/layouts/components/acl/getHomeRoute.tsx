339a9aab721058b9844971c8f644687e632c4d0b
Ali Drine <alidrine89@gmail.com>
2023-11-21 16:22:36 +0100

    fix the get logistique

--- src/layouts/components/acl/getHomeRoute.tsx
+++ src/layouts/components/acl/getHomeRoute.tsx
@@ -2,6 +2,8 @@
  *  Set Home URL based on User Roles
  */
 const getHomeRoute = (role: string) => {
+  console.log()
+
   if (role === 'instalateur') return '/instalateur/constructeur'
   else if (role === 'logisticien' || role === 'logistique') return '/logistique/inProgress'
   else return '/suivi-chantier'
