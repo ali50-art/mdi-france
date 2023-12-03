8a3fe277003e0815fbf905881bcfda1f4ca8fe09
Ali Drine <alidrine89@gmail.com>
2023-12-03 18:19:42 +0100

    save changing

--- src/views/apps/admin-stat/index.tsx
+++ src/views/apps/admin-stat/index.tsx
@@ -149,7 +149,7 @@ const SidebarAddUser = (props: SidebarAddUserType) => {
       <Dialog
         fullWidth
         open={open}
-        maxWidth='lg'
+        maxWidth={false}
         scroll='body'
         onClose={toggle}
         aria-labelledby='user-view-edit'
