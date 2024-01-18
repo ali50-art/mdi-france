9073c30075d1f06960d9e5d3dde05c9a1158c424
Ali Drine <alidrine89@gmail.com>
2024-01-18 17:03:44 +0100

    complete the landing-age

--- src/utils/generatePdfBackend.tsx
+++ src/utils/generatePdfBackend.tsx
@@ -131,12 +131,12 @@ const PDFGenerator = ({ data, data2 }: any) => {
     })
     pdf.addPage()
     pdf.setFontSize(10)
-    pdf.text('Les éléments déclarés ci-dessus sont à titre indicatif', 30, 5)
+    pdf.text('Les éléments déclarés ci-dessus sont à titre indicatif', 30, 20)
     pdf.setFontSize(13)
-    pdf.text('Signature + Date + Cachet ', 42, 14)
-    pdf.text('Signature + Date + Cachet ', 210, 14)
-    pdf.text('Nom/Prénom/Fonction du représentant : ', 30, 20)
-    pdf.text('Nom/Prénom/Fonction du bénéficiare : ', 200, 20)
+    pdf.text('Signature + Date + Cachet ', 42, 34)
+    pdf.text('Signature + Date + Cachet ', 210, 34)
+    pdf.text('Nom/Prénom/Fonction du représentant : ', 30, 40)
+    pdf.text('Nom/Prénom/Fonction du bénéficiare : ', 200, 40)
 
     // Save the PDF using save() method
     pdf.save('RES/TER.pdf')
